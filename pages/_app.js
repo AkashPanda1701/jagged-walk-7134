7a3a1332575a921ee6cbff5e28f6005de06abf59
Janhaviprabhu <januprabhu26@gmail.com>
2022-12-14 22:06:43 +0530

    navbar done

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import { store } from '../redux/store'
+import "../Components/Navbar/nav.css";
 
 // Don't make any changes to this file. 
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import Navbar from "../Components/Navbar/Navbar";
+import {Homecomp} from "../Components/Home/Home"
 
 
 export default function Home() {
   return (
-    <div >
-      Home Page
-    </div>
-  )
+    <>
+      <Navbar />
+      <Homecomp/>
+    </>
+  );
 }
