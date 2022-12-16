126bb8619790e0f2f84969dba807bf161f70b6dc
Mohd Monish <97455677+Mohd2002Monish@users.noreply.github.com>
2022-12-16 23:30:08 +0530

    Auth with Firebase

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
 import Navbar from "../Components/Navbar/Navbar";
-import Homecomp from "../Components/Home/Home"
-import Footer  from "../Components/Footer/footer";
-
+// import Homecomp from "../Components/Home/Home";
+// import Footer from "../Components/Footer/footer";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Homecomp/>
-      <Footer/>
+      {/* <Homecomp/>
+      <Footer/> */}
     </>
   );
 }
