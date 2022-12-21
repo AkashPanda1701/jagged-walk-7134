267c0c8043d7426d29cd1ff8dd5ebbb19ad9b6c5
AkashPanda1701 <akashpanda1701@gmail.com>
2022-12-21 16:53:22 +0530

    deleted app

--- middleware/connectDB.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-
-const connectDB = async (req, res) => {
-  return mongoose.connect('mongodb+srv://Akash:852654@cluster0.9xzz5v6.mongodb.net/pharma?retryWrites=true&w=majority')
-};
-
-export default connectDB;
